16d83ebce6c5c27861898b1178b8fb3e99425f18
Israel Zurita <israel.5k@hotmail.com>
2018-08-14 00:27:27 -0500

    Vista escoger formulario

--- EducateMovilApp/app/src/main/java/com/example/root/educateappcontrolvisitas/ui/CheckInActivity.java
+++ EducateMovilApp/app/src/main/java/com/example/root/educateappcontrolvisitas/ui/CheckInActivity.java
@@ -3,6 +3,10 @@ package com.example.root.educateappcontrolvisitas.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.root.educateappcontrolvisitas.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,6 +19,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class CheckInActivity extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
 
+    private RelativeLayout visitaTextContainer;
+    private TextView visitaHora;
+    private TextView visitaNumero;
+    private TextView escuelaMIE;
+    private TextView escuelaJornada;
+    private TextView escuelaNombre;
+    private TextView escuelaDireccion;
+    private Button button;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +38,25 @@ public class CheckInActivity extends AppCompatActivity implements OnMapReadyCall
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.g_map);
         mapFragment.getMapAsync(this);
+
+        visitaTextContainer = (RelativeLayout) findViewById(R.id.visita_text_container);
+        visitaHora = (TextView) findViewById(R.id.visita_hora);
+        visitaNumero = (TextView) findViewById(R.id.visita_numero);
+        escuelaMIE = (TextView) findViewById(R.id.escuela_MIE);
+        escuelaJornada = (TextView) findViewById(R.id.escuela_jornada);
+        escuelaNombre = (TextView) findViewById(R.id.escuela_nombre);
+        escuelaDireccion = (TextView) findViewById(R.id.escuela_direccion);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EscogerFormularioFragment escogerFormularioFragment = new EscogerFormularioFragment();
+                
+            }
+        });
+       
+
     }
 
     @Override
---
+++ EducateMovilApp/app/src/main/java/com/example/root/educateappcontrolvisitas/ui/TecnicoFormularioActivity.java
@@ -0,0 +1,19 @@
+package com.example.root.educateappcontrolvisitas.ui;
+
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.root.educateappcontrolvisitas.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TecnicoFormularioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tecnico_formulario);
+    }
+
+
+}
